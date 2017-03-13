73c58adae03a79cc29469a25aefba2c44851ff00
孙遥 <sunyao@leomaster.com>
2017-03-13 17:57:26 +0800

    fix bug

--- ratelimiter/README.md
+++ ratelimiter/README.md
@@ -2,5 +2,6 @@
 [![License](http://img.shields.io/npm/l/redis-rate-limiter.svg?style=flat)](https://github.com/TabDigital/redis-rate-limiter)
 #Description
 一个简单的限流组件基于promise方法，用来针对http请求进行次数限制。支持redis或者内存作为存储。
+
 #Useage
 待补全
--- ratelimiter/index.js
+++ ratelimiter/index.js
@@ -5,7 +5,8 @@ const redis  = require('redis');
 const assert = require('assert');
 
 module.exports = Limiter;
-
+let timeouts      = {};
+let storage       = {};
 /**
  *
  * @param opts
@@ -16,38 +17,32 @@ module.exports = Limiter;
  * @constructor
  */
 function Limiter(opts) {
-    this.id = opts.id;
-    this.db = opts.db;
+    let id = opts.id;
+    let db = opts.db;
+    let maxInInterval = opts.maxInInterval;
+    let interval      = opts.interval;
+    let key           = 'limiter:sets:' + id;
+    assert(id, '.id required');
+    assert(maxInInterval, '.maxInInterval required');
+    assert(interval, '.interval required');
 
-    //assert(this.db, '.db required');
-    this.timeouts      = {};
-    this.storage       = {};
-    this.maxInInterval = opts.maxInInterval;
-    this.interval      = opts.interval;
-    this.key           = 'limiter:sets:' + this.id;
-    assert(this.id, '.id required');
-    assert(this.maxInInterval, '.maxInInterval required');
-    assert(this.interval, '.interval required');
-}
-
-Limiter.prototype.info = function () {
     var now   = Date.now();
-    var start = now - this.interval * 1000;
+    var start = now - interval * 1000;
     return new Promise((resolve, reject) => {
-        if (this.db) {
-            this.db.multi()
-                .zremrangebyscore(this.key, now, start)//clear expired data
-                .zrange(this.key, 0, -1)
-                .zadd(this.key, now, now)
-                .expire(this.key, this.interval)
+        if (db) {
+            db.multi()
+                .zremrangebyscore(key, now, start)//clear expired data
+                .zrange(key, 0, -1)
+                .zadd(key, now, now)
+                .expire(key, interval)
                 .exec((err, result) => {
                     if (err) {
                         return reject(err);
                     }
                     let count                = result[1].length;
                     let timeSinceLastRequest = Array.isArray(result[1]) && result[1].length > 0 ? now - result[1][result[1].length - 1] : 0;
-                    let remaining            = (this.maxInInterval - count) < 0 ? -1 : this.maxInInterval - count;
-                    let reset                = Array.isArray(result[1]) && result[1].length > 0 ? parseInt(result[1][0]) + this.interval * 1000 : now + this.interval * 1000;
+                    let remaining            = (maxInInterval - count) < 0 ? -1 : maxInInterval - count;
+                    let reset                = Array.isArray(result[1]) && result[1].length > 0 ? parseInt(result[1][0]) + interval * 1000 : now + interval * 1000;
                     return resolve({
                         remaining           : remaining,
                         timeSinceLastRequest: timeSinceLastRequest,
@@ -55,18 +50,18 @@ Limiter.prototype.info = function () {
                     });
                 });
         } else {
-            clearTimeout(this.timeouts[this.id]);
-            let dataSet = this.storage[this.id] = (this.storage[this.id] || []).filter((timestamp) => {
+            clearTimeout(timeouts[id]);
+            let dataSet = storage[id] = (storage[id] || []).filter((timestamp) => {
                 return timestamp > start;
             });
             let count                = dataSet.length;
             let timeSinceLastRequest = Array.isArray(dataSet) && dataSet.length > 0 ? now - dataSet[dataSet.length - 1] : 0;
-            let remaining            = (this.maxInInterval - count) < 0 ? -1 : this.maxInInterval - count;
-            let reset                = Array.isArray(dataSet) && dataSet.length > 0 ? parseInt(dataSet[0]) + this.interval * 1000 : now + this.interval * 1000;
+            let remaining            = (maxInInterval - count) < 0 ? -1 : maxInInterval - count;
+            let reset                = Array.isArray(dataSet) && dataSet.length > 0 ? parseInt(dataSet[0]) + interval * 1000 : now + interval * 1000;
             dataSet.push(now);
-            this.timeouts[this.id] = setTimeout(() => {
-                delete this.storage[this.id];
-            }, this.interval);
+            timeouts[id] = setTimeout(() => {
+                delete storage[id];
+            }, interval*1000);
             resolve({
                 remaining           : remaining,
                 timeSinceLastRequest: timeSinceLastRequest,
@@ -75,6 +70,7 @@ Limiter.prototype.info = function () {
         }
     });
 }
+
 
 
 
